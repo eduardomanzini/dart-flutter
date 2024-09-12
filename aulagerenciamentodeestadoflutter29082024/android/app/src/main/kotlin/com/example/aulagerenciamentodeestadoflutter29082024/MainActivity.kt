a6df005dac16fbc2ff6d34c97b6153e7ea55cfb7
eduardomanzini <eduardo_manzini@hotmail.com>
2024-09-12 17:54:15 -0300

    gereciamento de estado setState

---
+++ aulagerenciamentodeestadoflutter29082024/android/app/src/main/kotlin/com/example/aulagerenciamentodeestadoflutter29082024/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.aulagerenciamentodeestadoflutter29082024
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ aulagerenciamentodeestadoflutter29082024/lib/main.dart
@@ -0,0 +1,122 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'To-Do List',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: TodoListScreen(),
+    );
+  }
+}
+
+class TodoListScreen extends StatefulWidget {
+  @override
+  _TodoListScreenState createState() => _TodoListScreenState();
+}
+
+class _TodoListScreenState extends State<TodoListScreen> {
+  final TextEditingController _taskController = TextEditingController();
+  List<Task> _tasks = [];
+
+  @override
+  void dispose() {
+    _taskController.dispose();
+    super.dispose();
+  }
+
+  void _addTask(String taskTitle) {
+    if (taskTitle.isNotEmpty) {
+      setState(() {
+        _tasks.add(Task(title: taskTitle));
+      });
+      _taskController.clear();
+    }
+  }
+
+  void _toggleTaskCompletion(int index) {
+    setState(() {
+      _tasks[index].isCompleted = !_tasks[index].isCompleted;
+    });
+  }
+
+  void _removeTask(int index) {
+    setState(() {
+      _tasks.removeAt(index);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('To-Do List'),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _taskController,
+                    decoration: InputDecoration(
+                      labelText: 'Add a task',
+                    ),
+                  ),
+                ),
+                IconButton(
+                  icon: Icon(Icons.add),
+                  onPressed: () => _addTask(_taskController.text),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: _tasks.length,
+              itemBuilder: (context, index) {
+                final task = _tasks[index];
+                return ListTile(
+                  title: Text(
+                    task.title,
+                    style: TextStyle(
+                      decoration: task.isCompleted
+                          ? TextDecoration.lineThrough
+                          : TextDecoration.none,
+                    ),
+                  ),
+                  leading: Checkbox(
+                    value: task.isCompleted,
+                    onChanged: (value) {
+                      _toggleTaskCompletion(index);
+                    },
+                  ),
+                  trailing: IconButton(
+                    icon: Icon(Icons.delete),
+                    onPressed: () => _removeTask(index),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Task {
+  String title;
+  bool isCompleted;
+
+  Task({required this.title, this.isCompleted = false});
+}
